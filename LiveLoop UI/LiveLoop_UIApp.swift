ebaadc2720e08b4f13c9130c921c5128298bbfef
Bilal Akram <work.bilalakram@gmail.com>
2025-09-16 17:15:27 +0200

    Initial Commit

---
+++ LiveLoop UI/LiveLoop_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveLoop_UIApp.swift
+//  LiveLoop UI
+//
+//  Created by Bilal Akram on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveLoop_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
